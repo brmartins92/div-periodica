ecdab73f1c98610c9f6678b5888bcfe78ee327bc
Bruno Martins <brmartins92@gmail.com>
2015-06-30 16:50:35 -0300

    logica acertada de esconder e aparecer elementos

--- script.js
+++ script.js
@@ -11,35 +11,43 @@ window.onload = function(){
 
 function metais_Alcalinos(){
 	metais_alcalinos.onmouseover = function(){
-    hidden_all_elemento('metais_alcalinos');
+    hidden_all_elemento('metais_alcalinosss');
   }
   tabela.onmouseover = function(){
-    var getClass = document.getElementsByClassName("nao_metais");
-    for(i=1;i<getClass.length;i++)
-    {
-     document.getElementsByClassName("nao_metais")[i].style.visibility = "visible";
-   }
- }
- div_legend_estado.onmouseover = function(){
-  var getClass = document.getElementsByClassName("nao_metais");
-  for(i=1;i<getClass.length;i++)
-  {
-   document.getElementsByClassName("nao_metais")[i].style.visibility = "visible";
- }
-}
+    show_all_elemento();
+  }
+  div_legend_estado.onmouseover = function(){
+    show_all_elemento(); 
+  }
 }
 
 function hidden_all_elemento(valor){
-  if(valor != 'nao_metais'){
-    var getClass = document.getElementsByClassName("nao_metais");
-    for(i=1;i<getClass.length;i++){
-     document.getElementsByClassName("nao_metais")[i].style.visibility = "hidden";
-    }
-  }
-  if(valor != 'metais_alcalinos'){
-    var getClass = document.getElementsByClassName("metais_alcalinos");
-    for(i=1;i<getClass.length;i++){
-     document.getElementsByClassName("metais_alcalinos")[i].style.visibility = "hidden";
-    }
+
+  var elementos = [
+    "nao_metais",
+    "metais_alcalinos"
+  ];
+
+  for(j=0;j<elementos.length;j++){
+    if(elementos[j] != valor){
+      var getClass = document.getElementsByClassName(elementos[j]);
+      for(i=1;i<getClass.length;i++){
+         document.getElementsByClassName(elementos[j])[i].style.visibility = "hidden";
+      } 
+    }   
   }  
+}
+
+function show_all_elemento(){
+    
+    var elementos = [
+    "nao_metais",
+    "metais_alcalinos"
+    ];
+    for(j=0;j<elementos.length;j++){
+    var getClass = document.getElementsByClassName(elementos[j]);
+      for(i=1;i<getClass.length;i++){
+         document.getElementsByClassName(elementos[j])[i].style.visibility = "visible";
+      }  
+    }  
 }
