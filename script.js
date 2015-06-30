26b46d237ea8f032d2086b3e574158d3f297473a
Bruno Martins <brmartins92@gmail.com>
2015-06-30 01:36:43 -0300

    ajustando js

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+  
+  var pag = window.location.pathname;
+  pag = pag.split("/");
+  switch(pag[5]){
+    case 'index.html':
+    	legendas();
+    break; 
+  }
+}
+
+function legendas(){
+	metais_alcalinos.onmouseover = function(){
+		
+	}
+}
