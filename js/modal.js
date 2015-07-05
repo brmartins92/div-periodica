633f68f987fc0021c05dcab301251fdc9ac3cb2b
Bruno Martins <brmartins92@gmail.com>
2015-07-04 22:35:07 -0300

    acertando modal

---
+++ js/modal.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  var pag = window.location.pathname;
+  pag = pag.split("/");
+  switch(pag[5]){
+    case 'index.html':
+    modal();
+    break; 
+  }
+});
+
+function modal(){
+  $('#btn_h').click(function(e) {
+    e.preventDefault();
+    $('#dialog').fadeIn(2000);
+  });
+
+  $('.window .close').click(function (e) {
+    e.preventDefault();
+    $('.window').hide();
+  });
+}
+
