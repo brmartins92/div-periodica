b99a970259b57c78abc002126ece384d704e0cd2
Bruno Martins <brmartins92@gmail.com>
2015-07-09 01:27:03 -0300

    criando o modal par ao litio

--- js/modal.js
+++ js/modal.js
@@ -33,7 +33,34 @@ function modal(){
     $('#modal_condutividade_aletrica').val("-");
     $('#modal_configuracao_eletronica').val("1s1");
     $('#modal_orbital').val("1s1");
-   abrir_modal();
+    abrir_modal();
+  });
+  
+  $('#btn_li').click(function(e) {
+    e.preventDefault();
+    $('#title_modal').text('Lítio');
+    $('#modal_num_atomico').val('3');
+    $('#modal_img').html("<img src='imagens/litio.jpg' id='img_modal'>");
+    $('#modal_origem').val('Natural');
+    $('#modal_descoberto').val("August Arfvedson / 1818");
+    $('#modal_origem_nome').val("Do grego Lithos, 'pedra'");
+    $('#modal_massa_atomica').val("[6,941(2)]");
+    $('#modal_volume_atomico').val("13,1 cm³/mol");
+    $('#modal_densidade').val("0,53 g/cm³");
+    $('#modal_fusao').val("180,54 °C");
+    $('#modal_ebulicao').val("1347 °C");
+    $('#modal_cntp').val("Sólido");
+    $('#modal_rede_critalografica').val("Cúbica de Corpo Centrado");
+    $('#modal_raio_atomico').val("1,55 Å");
+    $('#modal_eletronegatividade').val("0,98");
+    $('#modal_ionizacao').val("5,392 V");
+    $('#modal_raio_covalente').val("1,23 Å");
+    $('#modal_raio_ionico').val("0,60E+1 Å");
+    $('#modal_camadas').val("2,1");
+    $('#modal_condutividade_aletrica').val("11,7 E6/ohms/m");
+    $('#modal_configuracao_eletronica').val("[He] 2s1");
+    $('#modal_orbital').val("2s1");
+    abrir_modal();
   });
 
   $('.window .close').click(function (e) {
